b8db78c3a0c709d424c692c29b8365d005728bdc
Adrijana01 <adrijanapantic@gmail.com>
2023-12-16 23:26:59 +0100

    Adrijana test 4

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable!! JEBOTE
+     * The attributes that are mass assignable 
      *
      * @var array<int, string>
      */
