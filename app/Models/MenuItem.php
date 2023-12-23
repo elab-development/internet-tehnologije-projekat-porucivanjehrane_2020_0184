988455fc74f14a9a150cfb647d0a31616f43d01b
Ana Pavlovic <pavlovicana256@gmail.com>
2023-12-23 22:25:46 +0100

    MenuItem migration, seeders and factory

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -5,16 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Menu extends Model
+class MenuItem extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name'
+        'name',
+        'description',
+        'price',
+        'menu_id'
     ];
 
-    public function menu_items()
+    public function menu()
     {
-        return $this->hasMany(MenuItem::class);
+        return $this->belongsTo(Menu::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'address'
+        'address',
+        'role_id'
     ];
 
     /**
