8461fe9e5649ba1b8f5e7d1cbc3701f58f240232
Ana Pavlovic <pavlovicana256@gmail.com>
2023-12-23 22:43:36 +0100

    Order and OrderItem  migration, seeders and factory

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'quantity',
+        'order_id',
+        'menu_item_id',
+    ];
+
+    public function menuItem()
+    {
+        return $this->belongsTo(MenuItem::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
---
+++ database/migrations/2023_12_23_212625_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number')->unique();
+            $table->enum('status', ['pending', 'processing', 'completed', 'decline'])->default('pending');
+            $table->integer('item_count');
+            $table->float('price_total');
+            $table->boolean('is_paid')->default(false);
+            $table->enum('payment_method', ['cash_on_delivery'])->default('cash_on_delivery');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
