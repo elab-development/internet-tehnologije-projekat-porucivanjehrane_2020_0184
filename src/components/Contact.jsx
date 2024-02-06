dca2ad542c6e8fd3bee325cfdc75e1dfbbd9d1f5
Ana Pavlovic <pavlovicana256@gmail.com>
2024-02-06 21:46:27 +0100

    Adding a map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Map from "./Map";
 
 const Contact = () => {
   const handleSendMessage = (event) => {
@@ -17,24 +18,29 @@ const Contact = () => {
   };
   return (
     <div>
-      <h3 className="contactTitle">Contact us</h3>
-      <form className="formClass">
-        <div className="input__container">
-          <label htmlFor="name">Your name:</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-        <div className="input__container">
-          <label htmlFor="email">Your email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div className="input__container">
-          <label htmlFor="message">Your message:</label>
-          <textarea id="message" name="message" rows="4" required></textarea>
-        </div>
-        <button className="buttonContact" onClick={handleSendMessage}>
-          Send the message :)
-        </button>
-      </form>
+      <div>
+        <h3 className="contactTitle">Contact us</h3>
+        <form className="formClass">
+          <div className="input__container">
+            <label htmlFor="name">Your name:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="input__container">
+            <label htmlFor="email">Your email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="input__container">
+            <label htmlFor="message">Your message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+          </div>
+          <button className="buttonContact" onClick={handleSendMessage}>
+            Send the message :)
+          </button>
+        </form>
+      </div>
+      <div>
+        <Map />
+      </div>
     </div>
   );
 };
